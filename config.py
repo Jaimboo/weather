8199f0ce9189e3020dbb9ab911a22515c44b26b9
jaimboo <666maxy.mr@gmail.com>
2022-12-14 19:35:10 +0100

    config.py now create instance folder and cfg file if they do not exists yet index.html now show a simple widget with basic information

--- config.py
+++ config.py
@@ -6,9 +6,14 @@ from flask import Flask, current_app
 
 def init_cfg():
     cfg = {
-            'SECRET_KEY' : 'prova2',
+            'SECRET_KEY' : 'dev',
             'DATABASE' : os.path.join(current_app.instance_path, "weather.sqlite") 
         }
-    with open(os.path.join(current_app.instance_path, 'config.cfg'), 'a') as f:
-        for key, value in cfg.items():
-            f.write(f'{key} = "{value}"\n')
+    try:
+        os.makedirs(current_app.instance_path)
+        with open(os.path.join(current_app.instance_path, 'config.cfg'), 'a') as f:
+            for key, value in cfg.items():
+                f.write(f'{key} = "{value}"\n')
+    except OSError:
+        pass
+
