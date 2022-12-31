388e5718fe19e8923ecf5fd64f0c84048d4374d0
jaimboo <666maxy.mr@gmail.com>
2022-12-31 10:40:10 +0100

    created api request updated layout search form created a results page

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, flash, g, redirect, render_template, request, url_for
+from flask import Blueprint, flash, g, jsonify, redirect, render_template, current_app, request, url_for, session
 
 from werkzeug.exceptions import abort
 
@@ -6,19 +6,32 @@ from weather.auth import login_required
 from weather.db import get_db
 
 import json
-from datetime import datetime
+import requests
+import os
+
+from datetime import datetime, timedelta, timezone
 
 bp = Blueprint('api', __name__)
 
 @bp.route('/')
 def index():
-    with open('weather/placeholder.json', 'r') as f:
+    if session.get('city') is None:
+        session['city'] = 'Rome'
+
+    get_api(session['city'])
+
+    with open(os.path.join(current_app.instance_path, 'api_response.json'), 'r') as f:
         data = json.load(f)
-    return render_template('index.html', data=data)
+        d = {
+            'location': data['location'],
+            'current': data['current'],
+            'alerts': data['alerts']
+        }
+    return render_template('index.html', data=d)
 
 @bp.route('/days')
 def days():
-    with open('weather/placeholder.json', 'r') as f:
+    with open(os.path.join(current_app.instance_path, 'api_response.json'), 'r') as f:
         data = json.load(f)
 
         d = {
@@ -31,15 +44,32 @@ def days():
             
     return render_template('days.html', data=d)
 
-@bp.route('/days/<int:id>/')
-def hourly(id):
-    with open('weather/placeholder.json', 'r') as f:
-        data = json.load(f)
-        d = {
-            key:data[key] for key in [data['location'], data['forecast']['forecastday'][id]]
-        }
 
-    return render_template('hourly.html', data=d)
+@bp.route('/search', methods=['GET'])
+def search():
+    q = request.args.get('q')
+    data = search_api(q)
+    return render_template('results.html', data=data)
+
+@bp.route('/search/<string:city>')
+def set_city(city):
+    session['city'] = city
+    return redirect(url_for('api.index'))
+
+def get_api(city):
+    if os.path.exists(os.path.join(current_app.instance_path, 'api_response.json')) == False:
+        session.clear()
+    if session.get('last_u') is None or session['last_u'] + timedelta(minutes = 15) < datetime.utcnow().replace(tzinfo=timezone.utc):
+        print('updated')
+        data = requests.get(f'http://api.weatherapi.com/v1/forecast.json?key={current_app.config["SECRET_KEY"]}&q={city}&days=3&aqi=no&alerts=yes')
+        session['last_u'] = datetime.utcnow()
+        with open(os.path.join(current_app.instance_path, 'api_response.json'), 'w') as f:
+            f.truncate()
+            json.dump(data.json(), f)
+
+def search_api(q):
+    response = requests.get(f'http://api.weatherapi.com/v1/search.json?key={current_app.config["SECRET_KEY"]}&q={q}')
+    return response.json()
 
 @bp.app_template_filter()
 def datetimeformat(value, format='%Y/%m/%d %H:%M'):
